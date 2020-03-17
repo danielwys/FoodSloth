3ae075fd0071a94e9dd739915e352a4557fba292
SebastianLie <jackcass629@gmail.com>
2020-03-17 11:01:05 +0800

    Add website template

--- app.js
+++ app.js
@@ -8,34 +8,27 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var port = process.env.PORT || "8000";
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+app.get("/user", (req, res) => {
+  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
-
-module.exports = app;
