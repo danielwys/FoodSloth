205638ea1bf71b265f5433239d0a732e55e99e3c
tharshita <>
2020-03-17 23:02:23 +0800

    added backend folder with index.js and queries.js

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const bodyParser = require('body-parser')
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+const db = require('./queries')
+/**
+ *  App Configuration
+ */
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+/**
+ * Routes Definitions
+ */
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
+
