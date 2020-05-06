d4d7c14b032d2dc7cad997a4638fdc46dd590675
tharshita <>
2020-05-06 23:03:58 +0800

    added quantity field

--- backend/sql/tables.sql
+++ backend/sql/tables.sql
@@ -144,8 +144,10 @@ create table OrderTimes (
 create table OrderItems (
     orderId             integer,
     foodId              integer not null,
+    quantity            integer not null default 1,
     primary key(orderId, foodId),
-    foreign key (orderId) references Orders (orderId)
+    foreign key (orderId) references Orders (orderId),
+    constraint quantityAmt check (quantity > '$0')
 );
 
 create table Reviews (
